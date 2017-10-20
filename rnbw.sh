26ff1bf8c0d6b30b1f256b8ac8737adf9c33dede
bugdie4k <fedorough@gmail.com>
2017-10-20 03:21:30 +0300

    rnbw.sh

---
+++ rnbw.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ "$1" != "" ] && [ "$1" != "-" ]; then _from=$1;  else _from=17;  fi
+if [ "$2" != "" ] && [ "$2" != "-" ]; then _to=$2;    else _to=231;   fi
+if [ "$3" != "" ] && [ "$3" != "-" ]; then _how=$3;   else _how=0;    fi
+if [ "$4" != "" ] && [ "$4" != "-" ]; then _angle=$4; else _angle=60; fi
+if [ "$5" != "" ] && [ "$5" != "-" ]; then _width=$5; else _width=3;  fi
+
+echo ARGS USED:
+printf "%6s|%6s|%6s|%6s|%6s\n" from to how angle width
+printf "%6s|%6s|%6s|%6s|%6s\n" $_from $_to $_how $_angle $_width
+
+./rnbw $_from $_to $_how $_angle $_width
